66e3ac03602ab0829ed5fa71da54fd429a143528
Emmanuel Acevedo Díaz <eacevedod@uaemex.mx>
2023-02-28 16:59:29 -0600

    Angular material a lista de apps y credenciales

--- src/app/servidores/lista-apps/lista-apps.component.ts
+++ src/app/servidores/lista-apps/lista-apps.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Aplicacion } from 'src/app/models/aplicacion';
 import { ServidoresService } from 'src/app/services/api/servidores.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-lista-apps',
@@ -9,19 +11,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./lista-apps.component.css']
 })
 export class ListaAppsComponent implements OnInit {
-  aplicaciones!:Aplicacion[]|null;
+  aplicaciones:any;//Aplicacion[]|null;
   aplicacion!:Aplicacion|null;
+  frmApp:FormGroup;
 
   constructor(private servidoresService:ServidoresService,private route:ActivatedRoute,
     private router:Router){
     this.aplicacion={id:0,nombre:'',version:''}
+    this.frmApp = new FormGroup({
+      id: new FormControl(this.aplicacion.id),
+      nombre: new FormControl(this.aplicacion.nombre,[Validators.required]),
+      version: new FormControl(this.aplicacion.version,[Validators.required])
+    });
   }
 
   ngOnInit(){
     this.consulta();
   }
   guardar(){
+    if(!this.frmApp.valid )
+      return;
     let servidorId:number=this.route.snapshot.params['id'];
+    this.aplicacion = Object.assign({},this.frmApp.getRawValue());
     this.servidoresService.guardaAplicacion(servidorId,this.aplicacion).subscribe({
       next:(res) => {
         this.consulta();
@@ -33,10 +44,15 @@ export class ListaAppsComponent implements OnInit {
   editar(id:number){
     let servidorId:number=this.route.snapshot.params['id'];
     this.servidoresService.getAplicacion(servidorId,id).subscribe({
-      next: (res)=>{ this.aplicacion=res.body; },
+      next: (res)=>{
+        this.aplicacion=res.body;
+        let obj = Object.assign({},this.aplicacion);
+        this.frmApp.patchValue(obj);
+      },
       error: (e) => {console.log(e)}
     });
   }
+
   nueva(){
     this.aplicacion={id:0,nombre:'',version:''}
   }
@@ -44,7 +60,10 @@ export class ListaAppsComponent implements OnInit {
   consulta(){
     let id=this.route.snapshot.params['id'];
       this.servidoresService.getAplicaciones(id).subscribe({
-        next: (res) => {this.aplicaciones=res.body},
+        next: (res) => {
+          this.aplicaciones=res.body;
+          this.aplicaciones = new MatTableDataSource(this.aplicaciones);
+        },
         error: (e) => {console.log(e)}
       });
     this.aplicacion={id:0,nombre:'',version:''}
--- src/app/usuarios/lista-cred/lista-cred.component.ts
+++ src/app/usuarios/lista-cred/lista-cred.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Credencial } from 'src/app/models/credencial';
 import { ServidoresService } from 'src/app/services/api/servidores.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-lista-cred',
@@ -9,19 +11,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./lista-cred.component.css']
 })
 export class ListaCredComponent implements OnInit {
-  credenciales!:Credencial[]|null;
+  credenciales:any;//!:Credencial[]|null;
   credencial!:Credencial|null;
+  frmCred:FormGroup;
 
   constructor(private servidoresService:ServidoresService,private route:ActivatedRoute,
     private router:Router){
     this.credencial={id:0,login:'',password:'',usuario:0,aplicacion:null}
+    this.frmCred= new FormGroup({
+      id: new FormControl(this.credencial.id),
+      login: new FormControl(this.credencial.login,[Validators.required]),
+      password: new FormControl(this.credencial.password,[Validators.required]),
+    })
   }
   ngOnInit(): void {
       this.consulta();
   }
 
   guardar(){
+    if(!this.frmCred.valid )
+      return;
     let usuarioId:number=this.route.snapshot.params['id'];
+    this.credencial = Object.assign({},this.frmCred.getRawValue());
     this.servidoresService.guardaCredencial(usuarioId,this.credencial).subscribe({
       next:(res) => {
         this.consulta();
@@ -33,7 +44,11 @@ export class ListaCredComponent implements OnInit {
   editar(id:number){
     let usuarioId:number=this.route.snapshot.params['id'];
     this.servidoresService.getCredencial(usuarioId,id).subscribe({
-      next: (res)=>{ this.credencial=res.body; },
+      next: (res)=>{
+        this.credencial=res.body;
+        let obj = Object.assign({},this.credencial);
+        this.frmCred.patchValue(obj);
+      },
       error: (e) => {console.log(e)}
     });
   }
@@ -45,12 +60,13 @@ export class ListaCredComponent implements OnInit {
   consulta(){
     let id=this.route.snapshot.params['id'];
       this.servidoresService.getCredenciales(id).subscribe({
-        next: (res) => {this.credenciales=res.body},
+        next: (res) => {
+          this.credenciales=res.body;
+          console.log(this.credenciales);
+          this.credenciales = new MatTableDataSource(this.credenciales);
+        },
         error: (e) => {console.log(e)}
       });
       this.credencial={id:0,login:'',password:'',usuario:0,aplicacion:null}
   }
-
-
-
 }
